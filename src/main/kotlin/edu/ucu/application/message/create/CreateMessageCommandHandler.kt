f531dcaf804a1edf4248a46d685b28e7ae83c5af
Yaroslaww-1 <yaroslaww1@gmail.com>
2022-10-22 21:27:14 +0300

    added logging to CreateMessageCommandHandler.kt

--- src/main/kotlin/edu/ucu/application/message/create/CreateMessageCommandHandler.kt
+++ src/main/kotlin/edu/ucu/application/message/create/CreateMessageCommandHandler.kt
@@ -4,15 +4,20 @@ import edu.ucu.application.message.MessageRepository
 import edu.ucu.domain.message.Message
 import edu.ucu.infrastructure.DelayManager
 import io.micronaut.context.annotation.Prototype
+import org.slf4j.LoggerFactory
 
 @Prototype
 class CreateMessageCommandHandler(
     private val messageRepository: MessageRepository,
     private val delayManager: DelayManager
 ) {
+    private val logger = LoggerFactory.getLogger(CreateMessageCommandHandler::class.java)
+
     suspend fun execute(command: CreateMessageCommand): Unit {
+        logger.info("New message received with id = ${command.id}")
         delayManager.delayIfEnabled()
         val message = Message.create(command.id, command.messageText, command.createdAt)
         messageRepository.add(message)
+        logger.info("Message saved with id = ${command.id}")
     }
 }
